9dc198b938953c373ec8ec0eead36f9d066eefce
Nafeesa Hamid <148888825+FFG34@users.noreply.github.com>
2023-12-27 23:11:34 +0530

    implented the desired gui

---
+++ AES351/Form1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AES351
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form load
+        }
+
+        private void click_RunButton(object sender, EventArgs e)
+        {
+            // Button "Run"
+        }
+
+        private void click_SyntaxButton(object sender, EventArgs e)
+        {
+            // Button "Syntax"
+        }
+
+        private void text_CommandTextBox(object sender, EventArgs e)
+        {
+            // Text box "Command"
+        }
+
+        private void click_Display(object sender, EventArgs e)
+        {
+            // Picture box for display
+        }
+    }
+}
