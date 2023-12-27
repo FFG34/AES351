ec2adc7ab1d911de508719b769b5f551ba56bc4b
Nafeesa Hamid <148888825+FFG34@users.noreply.github.com>
2023-12-28 00:40:13 +0530

    commandparser half done

---
+++ AES351/CommandParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+public class CommandParser
+{
+    private TextBox programInputBox;
+    private PictureBox canvasDisplay;
+
+    public CommandParser(TextBox programInputBox, PictureBox canvasDisplay)
+    {
+        this.programInputBox = programInputBox;
+        this.canvasDisplay = canvasDisplay;
+    }
+
+    // Handles the execution of an individual command
+    public void ProcessCommand(string commandLine)
+    {
+        // Implement command processing and execution logic
+    }
+
+    // Processes a batch of commands from the program input area
+    public void RunProgram()
+    {
+        var lines = programInputBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            ProcessCommand(line.Trim());
+        }
+    }
+
+    // Records the program from the input area to a file
+    public void ArchiveProgram(string targetPath)
+    {
+        File.WriteAllText(targetPath, programInputBox.Text);
+    }
+
+    // Retrieves a program from a file and places it into the input area
+    public void RetrieveProgram(string sourcePath)
+    {
+        programInputBox.Text = File.ReadAllText(sourcePath);
+    }
+
+    // Validates the syntax in the program input area
+    public void ValidateSyntax()
+    {
+        var lines = programInputBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            // Implement the validation logic for the syntax
+            // Example structure - replace with actual checks
+            // if (!IsCommandValid(line.Trim()))
+            // {
+            //     throw new ArgumentException($"Incorrect syntax in line: {line}");
+            // }
+        }
+        // Inform the user if syntax validation passed
+        MessageBox.Show("All lines are syntactically correct.", "Syntax Validation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    // Checks if the syntax of a command is correct
+    private bool IsCommandValid(string commandLine)
+    {
+        // Replace with actual validation checks
+        return true; // this is a placeholder
+    }
+}
